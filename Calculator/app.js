901c36fc7bd3f576e91be3a98b8ba4f221367838
Areebey <areebrafiq125@gmail.com>
2023-10-23 04:02:24 +0500

    Calculator Completed

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,2 +1,37 @@
 // Here We Can COde for making a simple calculator App
 
+function appendToDisplay(value){
+const display=document.getElementById('display');
+ display.value+=value;
+}
+
+function clearDisplay(value){
+const display=document.getElementById('display');
+ display.value ='';
+}
+
+function calculate(){
+    const display=document.getElementById('display')
+    try{
+        display.value=eval(display.value);
+    }catch(error){
+        display.value='Error';
+    }
+}
+
+
+ const buttons=document.querySelectorAll('button');
+
+ buttons.forEach((button)=>{
+ button.addEventListener('click',()=>{
+
+    if(button.textContent==='Clear'){
+        clearDisplay();
+    }
+    else if(button.textContent==='='){
+        calculate();
+    }else{
+        appendToDisplay(button.textContent);
+    }
+ })
+})
